4f8a4d475838e69108dcb7cead075fcc1f076586
Ultimatecodder <86732126+Ultimatecodder@users.noreply.github.com>
2021-07-14 16:49:25 +0530

    Add files via upload

---
+++ projectt template/sketch.js
@@ -0,0 +1,22 @@
+var bomb;
+var path;
+var power;
+var runner;
+function preload(){
+  //pre-load images
+bomb.addAnimation=("bomb");
+path.addAnimation=("path")
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+bomb=createSprite=(300,300,10,10);
+path=createSprite=(200,200,20,20);
+power=createSprite=(100,100,30,30);
+}
+
+function draw() {
+  background(0);
+
+}
